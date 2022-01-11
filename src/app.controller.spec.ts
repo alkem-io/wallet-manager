929954154e653c5322f56e4cb2024e2713c1a778
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2022-01-11 18:43:46 +0200

    Fixed CI

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,11 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { AppController } from './app.controller';
+import { SsiAgentService } from './services/agent/ssi.agent.service';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: WINSTON_MODULE_NEST_PROVIDER,
+          useValue: {
+            error: jest.fn(),
+            verbose: jest.fn(),
+          },
+        },
+        {
+          provide: SsiAgentService,
+          useValue: {
+            createAgent: jest.fn(),
+            getVerifiedCredentials: jest.fn(),
+            grantCredential: jest.fn(),
+          },
+        },
+      ],
       controllers: [AppController],
     }).compile();
 
