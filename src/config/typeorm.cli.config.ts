4ce2c15b055cf34854a495638c0e0d937101ac64
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2022-03-28 15:03:18 +0300

    Updated config and migration config not to overlap with alkemio env vars

--- src/config/typeorm.cli.config.ts
+++ src/config/typeorm.cli.config.ts
@@ -3,12 +3,12 @@ import { join } from 'path';
 
 export const typeormCliConfig: ConnectionOptions = {
   type: 'mysql',
-  host: process.env.DATABASE_HOST ?? 'localhost',
+  host: process.env.JOLOCOM_DATABASE_HOST ?? 'localhost',
   port: process.env.MYSQL_DB_PORT ? Number(process.env.MYSQL_DB_PORT) : 3306,
   cache: true,
   username: 'root',
   password: process.env.MYSQL_ROOT_PASSWORD ?? 'toor',
-  database: process.env.MYSQL_DATABASE ?? 'jolocom',
+  database: process.env.JOLOCOM_MYSQL_DATABASE ?? 'jolocom',
   insecureAuth: true,
   synchronize: false,
   logger: 'advanced-console',
